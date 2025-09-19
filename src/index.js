2b01f2f74ca7a14a5602e5ecb013341b173fcf32
piperpei <piperpei@yahoo.com>
2025-09-18 21:13:37 -0700

    Event Listener Forum

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault(); 
+
+    const taskInput = event.target.querySelector("#new-task-description").value;
+
+    buildToDo(taskInput); 
+
+    event.target.reset();    
+  }); 
 });
+
+function buildToDo(task) {
+  const li = document.createElement("li"); 
+  li.textContent = task; 
+
+  const taskList = document.getElementById("tasks"); 
+  taskList.appendChild(li); 
+}
